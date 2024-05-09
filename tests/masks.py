08963f4a72b1471c864780eedd6b6963aa84c2bf
Maxim <ggsansgg1@gmail.com>
2024-05-09 14:15:19 +0900

    all files

---
+++ tests/masks.py
@@ -0,0 +1,14 @@
+def masks_card(number_card: str) -> str:
+    """Принимает номер карты и возвращает ее маску"""
+    masks_number_card = number_card[:4] + " " + number_card[4:6] + "** **** " + number_card[12:16]
+    return masks_number_card
+
+
+def masks_score(number_score: str) -> str:
+    """Принимает номер счета и возвращает его маску"""
+    masks_number_score = "**" + number_score[-4:]
+    return masks_number_score
+
+
+print(masks_card("7000792289606361"))
+print(masks_score("73654108430135874305"))
