38b7632873a10231153201b0ee4c2a1b62183eb7
Maxim <ggsansgg1@gmail.com>
2024-05-11 00:49:04 +0900

    fix

--- src/widget.py
+++ src/widget.py
@@ -2,8 +2,13 @@ def mask_number(number: str) -> str:
     """Принимает на вход строку и делает маску"""
     for i in number:
         if (i == "с") or (i == "С"):
-            masks_number_score = number[:4] + "**" + number[-4:]
+            masks_number_score = number[:4] + " **" + number[-4:]
             return masks_number_score
         else:
-            masks_number_card = number[:9] + " " + number[9:11] + "** **** " + number[17:21]
-            return masks_number_card
+            a = number.split()
+            w = []
+            for s in a:
+                if s.isalpha() == True:
+                    w.append(s)
+            q = " ".join(w)
+            return q + " " + number[-19:-7] + "** ****" + number[-5:]
